755b22b838148aa15bc2c00589033d3780b7b061
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-03-28 14:55:38 -0400

    created initial divs in html and classes in css

---
+++ Assets/js/main.js
@@ -0,0 +1,22 @@
+//a timer counts down from 60 seconds to 0 
+
+//time is subtracted from the clock for incorrect answers
+
+//game stops when all questions are answered or time runs out
+
+//score = remaining seconds
+
+//can save initials and score at the end of the game (leaderboard)
+
+//start quiz button
+
+//questions
+
+//choices
+
+//feedback for correct and incorrect responses
+
+
+
+
+
