20ff102f78f64ac0ff3725c18d21fe19aeb12e5d
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-03-30 11:00:29 -0400

    adding back assets folder

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+//a timer counts down from 60 seconds to 0 
+
+//time is subtracted from the clock for incorrect answers
+
+//game stops when all questions are answered or time runs out
+
+//score = remaining seconds
+
+//can save initials and score at the end of the game (leaderboard)
+
+//start quiz button
+
+
+
+//feedback for correct and incorrect responses
+
+
+
+// arrays with questions, answer options, and correct answers.
+
+let questions = [
+    {
+    quest: "When was Lake Champlain discoverd by French explorer, Samuel de Champlain?",
+    options: ["1557", "1609", "1689", "1777"],
+    correctAnswer: "Lake Champlain discoverd by French explorer, Samuel de Champlain in 1609."
+},
+
+{
+    quest: "Some say a mysterious dinasaur-like creature lives in the lake. What is his name?",
+    options: ["Champ", "Loch Ness Monster", "Freddisaurus", "Sturgie the Sturgeon"],
+    correctAnswer: "Champ is the name of the creature who most certainly lives in Lake Champlain."
+},
+
+{
+    quest: "Which two states are connected by the Lake Champlain bridge?",
+    options: ["Washington & Minnesota", "Idaho & Minnesota", "New York & Vermont", "Maine & New Hampshire"],
+    correctAnswer: "The Lake Champlain bridge connects New York and Vermont."
+},
+
+{
+    quest: "The deepest point in the lake is between Charlotte, VT and Essex, NY. How many feet deep is this deepest point?",
+    options: ["126 feet", "348 feet", "400 feet", "1500 feet"],
+    correctAnswer: "The lake's deepest point is 400 feet."
+},
+
+{
+    quest: "How many species of fish are there in Lake Champlain",
+    options: ["25", "90", "200", "625"],
+    correctAnswer: "There are approximately 90 species of fish in Lake Champlain."
+},
+    ]
+
+
+
