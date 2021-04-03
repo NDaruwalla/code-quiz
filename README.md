79fc314bda21af6f1fabcc988650533af00349d4
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-04-03 15:01:54 -0400

    completed code for buttons, question and choice sections, began developing README

--- README.md
+++ README.md
@@ -1,29 +1,27 @@
-# 04 Web APIs: Code Quiz
+# 04 Web APIs: Code Quiz - Lake Champlain
 
-## Your Task
+## App Objective
 
-At some point in your journey to become a full-stack web developer, you’ll likely be asked to complete a coding assessment&mdash;perhaps as part of an interview process. A typical coding assessment includes both multiple-choice questions and interactive coding challenges. 
+The objective of this project is to demonstrate my ability to code both multiple-choice questions and an interactive leaderboard. 
 
-To help familiarize you with these tests and allow you to use the skills covered in this unit, this week’s homework invites you to build a timed coding quiz with multiple-choice questions. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean, polished, and responsive user interface. 
-
-This week’s coursework will equip you with all the skills you need to succeed in this assignment.
+This timed coding quiz has multiple-choice questions. The app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code. It features a clean, polished, and responsive user interface. 
 
 ## User Story
 
 ```
-AS A coding boot camp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
+AS A coding website visitor
+I WANT to take a timed quiz about Lake Champlain that stores high scores
+SO THAT I can gauge my progress compared to others
 ```
 
-## Acceptance Criteria
+## Key Features
 
 ```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
+* The quiz features a start button
+* WHEN I click the start button
+* THEN a timer starts and I am presented with a question
+* WHEN I answer a question
+* THEN I am presented with another question
 WHEN I answer a question incorrectly
 THEN time is subtracted from the clock
 WHEN all questions are answered or the timer reaches 0
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,232 @@
-//a timer counts down from 60 seconds to 0 
+// a timer counts down from 60 seconds to 0 when start quiz button is clicked (event listener)
 
-//time is subtracted from the clock for incorrect answers
+// quiz begins (question presented) when start quiz button is clicked
 
-//game stops when all questions are answered or time runs out
+// time (5 seconds) is subtracted from the clock for each incorrect answer
 
-//score = remaining seconds
+// quiz stops when all questions are answered or timer reaches zero seconds remaining
 
-//can save initials and score at the end of the game (leaderboard)
+// score = remaining seconds 
 
-//start quiz button
+// can save initials and score at the end of the game (leaderboard)
 
+// user can clear the leaderboard (delete scores and initials)
 
+// from leaderboard user can restart quiz
 
-//feedback for correct and incorrect responses
+// from leaderboard page user can return to homepage
 
+// start quiz button
 
+// feedback for correct and incorrect responses
+
+// Resources reviewed: https://www.udemy.com/course/build-a-quiz-app-with-html-css-and-javascript/learn/lecture/13685348#overview
+
+let questionhtml = document.getElementById('questText');
+let choices1html = document.getElementById('choice1');
+let choices2html = document.getElementById('choice2');
+let choices3html = document.getElementById('choice3');
+let choices4html = document.getElementById('choice4');
+let startquizhtml = document.getElementById('startQuiz');
+let countdownhtml = document.getElementById('countdown');
+let feedbackhtml = document.getElementById('feedback');
+
+console.log(startquizhtml);
 
 // arrays with questions, answer options, and correct answers.
 
 let questions = [
-    {
-    quest: "When was Lake Champlain discoverd by French explorer, Samuel de Champlain?",
-    options: ["1557", "1609", "1689", "1777"],
-    correctAnswer: "Lake Champlain discoverd by French explorer, Samuel de Champlain in 1609."
-},
+  {
+    questNum: 1,
+    questText: 'In what year was Lake Champlain discovered by French explorer, Samuel de Champlain?',
+    choicesText: ['1557', '1609', '1689', '1777'],
+    correctAnswer: '1609'
+  },
 
+  {
+    questNum: 2,
+    questText: 'What is the name of the mysterious dinasaur-like creature who lives in Lake Champlain?',
+    choicesText: ['Champ', 'Freddisaurus', 'Loch Ness Monster', 'Sturgie the Sturgeon'],
+    correctAnswer: 'Champ'
+  },
+
+  {
+    questNum: 3,
+    questText: 'Which two states are connected by the Lake Champlain bridge?',
+    choicesText: ['Washington & Minnesota', 'Idaho & Minnesota', 'New York & Vermont', 'Maine & New Hampshire'],
+    correctAnswer: 'New York & Vermont'
+  },
+
+  {
+    questNum: 4,
+    questText: 'The deepest point in Lake Champlain is how many feet?',
+    choicesText: ['126', '348', '400', '1500'],
+    correctAnswer: '400'
+  },
+
+  {
+    questNum: 5,
+    questText: 'How many species of fish are there in Lake Champlain',
+    choicesText: ['25', '90', '200', '625'],
+    correctAnswer: '90'
+  }
+
+];
+
+let secondsLeft = 60;
+let timerBreak = 0;
+// let timerBreak = 0
+
+// skipping new element for now
+let score = 0;
+let questionIndex = 0;
+
+startquizhtml.addEventListener('click', function ()
+// document.getElementById("countdown").addEventListener('click', function() 
 {
-    quest: "Some say a mysterious dinasaur-like creature lives in the lake. What is his name?",
-    options: ["Champ", "Loch Ness Monster", "Freddisaurus", "Sturgie the Sturgeon"],
-    correctAnswer: "Champ is the name of the creature who most certainly lives in Lake Champlain."
-},
+  if (timerBreak === 0) {
+    timerBreak = setInterval(function () {
+      secondsLeft--
+      countdownhtml.textContent = 'Time: ' + secondsLeft
 
-{
-    quest: "Which two states are connected by the Lake Champlain bridge?",
-    options: ["Washington & Minnesota", "Idaho & Minnesota", "New York & Vermont", "Maine & New Hampshire"],
-    correctAnswer: "The Lake Champlain bridge connects New York and Vermont."
-},
+      if (secondsLeft <= 0) {
+        clearInterval(timerBreak)
+        // timerBreak = 1
+        countdownhtml.textContent = "You're out of time!"
+      }
+    }, 1000)
+  };
 
-{
-    quest: "The deepest point in the lake is between Charlotte, VT and Essex, NY. How many feet deep is this deepest point?",
-    options: ["126 feet", "348 feet", "400 feet", "1500 feet"],
-    correctAnswer: "The lake's deepest point is 400 feet."
-},
+  funcQuestions(0)
+});
 
-{
-    quest: "How many species of fish are there in Lake Champlain",
-    options: ["25", "90", "200", "625"],
-    correctAnswer: "There are approximately 90 species of fish in Lake Champlain."
-},
-    ]
+function funcQuestions (i) {
 
+  // for  (let i = 0; i <5; i++) 
 
+  questionIndex = i;
+
+  if (questionIndex > 4) {
+    secondsLeftfinal = secondsLeft;
+    clearInterval(timerBreak);
+    // prompt("Time up")
+    countdownhtml.textContent = 'Congratulations! Your score is: ' + String(secondsLeftfinal);
+    console.log(questionIndex);
+    secondsLeft = 60;
+    //clearInterval(timerBreak)
+    savetoleaderboard();
+    return;
+  };
+
+  let userQuestion = questions[i].questText;
+  questionhtml.textContent = userQuestion;
+  // console.log(userQuestion)
+
+  let userChoice1 = questions[i].choicesText[0];
+  choices1html.textContent = userChoice1;
+  // console.log(userChoice1)
+
+  let userChoice2 = questions[i].choicesText[1];
+  choices2html.textContent = userChoice2;
+  // console.log(userChoice2)
+
+  let userChoice3 = questions[i].choicesText[2];
+  choices3html.textContent = userChoice3;
+  // console.log(userChoice3)
+
+  // let userChoice4 = questions[i].choicesText[3]
+  // choices4html.textContent = userChoice4
+
+  choices4html.textContent = questions[i].choicesText[3];
+  // console.log(userChoice4)
+
+  console.log(questionIndex);
+  console.log(setInterval);
+  console.log(timerBreak);
+
+  // if (questions[0].correctAnswer ===  ){}
+
+};
+
+function choice1selectfunction () {
+  let userSelectedAnswer = questions[questionIndex].choicesText[0];
+  // console.log(userSelectedAnswer)
+  if (userSelectedAnswer === questions[questionIndex].correctAnswer) {
+    // console.log("You are correct")
+    feedbackhtml.textContent = 'You are correct!';
+    // i++
+    console.log(questionIndex);
+    funcQuestions(questionIndex + 1);
+    return;
+  };
+  if (userSelectedAnswer !== questions[questionIndex].correctAnswer) {
+    // console.log("You are wrong")
+    feedbackhtml.textContent = 'You are wrong, try again!';
+    secondsLeft = secondsLeft - 5;
+  };
+};
+
+function choice2selectfunction () {
+  let userSelectedAnswer = questions[questionIndex].choicesText[1];
+  // console.log(userSelectedAnswer)
+  if (userSelectedAnswer === questions[questionIndex].correctAnswer) {
+    // console.log("You are correct")
+    feedbackhtml.textContent = 'You are correct!';
+    // i++
+    console.log(questionIndex);
+    funcQuestions(questionIndex + 1);
+    return;
+  };
+  if (userSelectedAnswer != questions[questionIndex].correctAnswer) {
+    // console.log("You are wrong")
+    feedbackhtml.textContent = 'You are wrong, try again!';
+    secondsLeft = secondsLeft - 5;
+  };
+};
+
+function choice3selectfunction () {
+  let userSelectedAnswer = questions[questionIndex].choicesText[2];
+  // console.log(userSelectedAnswer)
+  if (userSelectedAnswer === questions[questionIndex].correctAnswer) {
+    // console.log("You are correct")
+    feedbackhtml.textContent = 'You are correct!';
+    // i++
+    console.log(questionIndex);
+    funcQuestions(questionIndex + 1);
+    return;
+  };
+  if (userSelectedAnswer != questions[questionIndex].correctAnswer) {
+    // console.log("You are wrong")
+    feedbackhtml.textContent = 'You are wrong, try again!';
+    secondsLeft = secondsLeft - 5;
+  };
+};
+
+function choice4selectfunction () {
+  let userSelectedAnswer = questions[questionIndex].choicesText[3];
+  // console.log(userSelectedAnswer)
+  if (userSelectedAnswer === questions[questionIndex].correctAnswer) {
+    // console.log("You are correct")
+    feedbackhtml.textContent = 'You are correct!';
+    // i++
+    console.log(questionIndex);
+    funcQuestions(questionIndex + 1);
+    return;
+  };
+  if (userSelectedAnswer != questions[questionIndex].correctAnswer) {
+    // console.log("You are wrong")
+    feedbackhtml.textContent = 'You are wrong, try again!';
+    secondsLeft = secondsLeft - 5;
+  };
+};
+
+choices1html.addEventListener('click', choice1selectfunction);
+choices2html.addEventListener('click', choice2selectfunction);
+choices3html.addEventListener('click', choice3selectfunction);
+choices4html.addEventListener('click', choice4selectfunction);
+
+function savetoleaderboard () {
+  console.log('Save to Leaderboard');
+  console.log('Your score was: ' + String(secondsLeft));
+};
 
